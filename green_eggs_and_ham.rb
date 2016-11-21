0ad4e1b91aefd36a20708bb4ce8958c1d5e5a7d4
Kenia Valladarez <keniavalladarez@gmail.com>
2016-11-21 16:06:59 -0500

    completed explorer mode

--- green_eggs_and_ham.rb
+++ green_eggs_and_ham.rb
@@ -3,4 +3,29 @@ class GreenEggsAndHam
     @text = File.read('green_eggs_and_ham.txt')
     @words = @text.split(/[ .!?,\n]/).reject { |word| word.empty? }
   end
+
+  # count_of_total_words
+  def word_count
+    @words.count
+  end
+
+  # all_unique_words_sorted
+  def sorted_unique_words
+    @words.map { |word| word.downcase }.uniq.sort
+  end
+
+  # count_of_words_less_than_four_letters
+  def number_of_words_shorter_than ( word )
+    @words.count {|word| word.length < 4}
+  end
+
+  # longest_word
+  def longest_word
+    @words.max { |word| word.length}
+  end
+
+  # how_many_stanzas
+  def stanzas
+    @text.split("\n\n")
+  end
 end
